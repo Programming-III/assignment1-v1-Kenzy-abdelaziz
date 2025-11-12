13373e3e1792e35e9070b993f0d5787d9de48e34
Kenzy-abdelaziz <kenzy.abdelaziz@student.giu-uni.de>
2025-11-12 05:27:23 -0800

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,20 @@
-#ifndef VISITOR_H
-#define VISITOR_H
-
+#ifndef VISITOR_H_INCLUDED
+#define VISITOR_H_INCLUDED
+#include <iostream>
 #include <string>
 using namespace std;
+#include "Animal.h"
+class Visitor:public Animal{
+private:
+    string visitorName;
+    int ticketsBought;
+public:
+    void displayInfo();
+    Visitor();
+    Visitor(string name, int age, bool isHungry, string visitorName, int ticketsBought);
+    ~Visitor();
 
-//define visitor class here
+};
 
 
-
-
-
-
-
-#endif
+#endif // VISITOR_H_INCLUDED
