cd5294cd6180d3f80df0062de2d87056f5ebda33
Kenzy-abdelaziz <kenzy.abdelaziz@student.giu-uni.de>
2025-11-12 05:36:13 -0800

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,21 @@
-#ifndef ENCLOSURE_H
-#define ENCLOSURE_H
-
+#ifndef ENCLOSURE_H_INCLUDED
+#define ENCLOSURE_H_INCLUDED
+#include <iostream>
+#include <string>
+using namespace std;
 #include "Animal.h"
+class Enclosure{
+private:
+    Animal*animal;
+    int capacity;
+    int currentCount;
+public:
+    void addAnimal();
+    void displayAnimals();
+    Enclosure();
+    Enclosure(string name, int age, bool isHungry, int capacity,int currentCount);
+    ~Enclosure();
+};
 
-//define enclosure class here 
 
-
-
-
-
-
-
-
-#endif
+#endif // ENCLOSURE_H_INCLUDED
